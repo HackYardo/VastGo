6f45cf229a52e92a5fed1befce52923d100f1863
Ya Tienna <57677458+HackYardo@users.noreply.github.com>
2022-03-13 08:18:39 +0800

    try functional

--- magnet.jl
+++ magnet.jl
@@ -1,15 +1,53 @@
-boardSize=[19,19]
-m = rand([-1,0,1,0,0,0], (boardSize[1],boardSize[2]))
-println(m)
-p = rand(boardSize[1]:boardSize[2], 2)
-c = rand([-1,1], 1)
-m = cat(p,c, dims=1)
-println(m)
-m[p[1],p[2]]=0
-i = 1
-m_vector = m 
-while p[2] < boardSize[2]
-	m[p[1],p[2]+i] == c
-	i = 1 + i
-	while m[p[1],p[2]+i] in [-1,1]
+function magnet_stones()
+    m[p[1],p[2]]=0
+    i = 1
+    moveVector = move 
+    first = true
+    while p[2] < boardSize[2]
+        next = m[p[1],p[2]+i]
+        if next == 0
+            continue
+        elseif next == -c
+            if i == 1
+                break
+            elseif first == true
+                moveVector = cat(moveVector,[p[1],p[2]+1,-c], dims=2)
+                m[p[1],p[2]+i] = 0
+                break
+            else
+                moveVector = cat(moveVector,[p[1],p[2]+i-1,c], dims=2)
+                break
+            end
+        else # move_next == c
+            if p[2]+i == boardSize[2]
+                break
+            elseif first == true 
+                m[p[1],p[2]+i] = 0  
+                first == false  
+            else    
+                moveVector = cat(moveVector,[p[1],p[2]+i-1,c], dims=2)
+                break
+            end
+        end
+        i = 1 + i
+    end
+    println(m)
+    println(moveVector)
+    println(first)
+    println(i)
+end
 
+function main()
+    boardSize=[19,19]
+    position = rand([-1,0,1], (boardSize[1],boardSize[2]))
+    println(position)
+    point = [0,0]
+    p[1] = rand(1:boardSize[1])
+    p[2] = rand(1:boardSize[2])
+    c = rand([-1,1], 1)
+    move = cat(p,c, dims=1)
+    println(move)
+    magnet_stones(m,)
+end
+
+main()
