046363134af548278c2c342631d3953e513ce38d
Ya Tienna <57677458+HackYardo@users.noreply.github.com>
2022-07-27 06:47:25 +0800

    auto recognize models line from text

--- kata1_models_curve.jl
+++ kata1_models_curve.jl
@@ -1,35 +1,109 @@
+#=
+    This code is for realizing Regex in action.
+    Just copy text from https://katagotraining.org/ to a file,
+    then run the code in terminal: 
+        cmd> julia models.jl path/to/file
+=#
+
 using Dash, PlotlyBase
 
-x = range(1, stop=200, length=30)
-function p(style)
-    Plot(
-    scatter(x=x, y=x.^3, range_x=[0.8, 250], mode="markers"),
-    Layout(
-        yaxis_type="linear",
-        xaxis_type=style,
-        xaxis_range=[log10(0.8), log10(250)]
-    )
-)
+file = ARGS[1]
+
+function get_trace(file::String)
+    lines = readlines(file)
+    linesMatch = match.(r"^k.{2,}\d{3,}\.\d",lines)
+    linesWithoutNothing = []
+    nameVector = []
+    xVector = []
+    yVector = []
+    traces = Dict()
+    for line in linesMatch
+        if isnothing(line)
+            continue
+        else
+            lineSplit = split(line.match)
+            nnStructure = match(r"b.{3,5}\d",lineSplit[1]).match
+            dataRows = parse(Int64, match(r"d.{2,}",lineSplit[1]).match[2:end])
+            elo = parse(Float64, lineSplit[end])
+            nameVector = cat(nameVector, nnStructure, dims=1)
+            xVector = cat(xVector, dataRows, dims=1)
+            yVector = cat(yVector, elo, dims=1)
+        end
+    end
+    linesWithoutNothing = (name=nameVector, x=xVector, y=yVector)
+    #println(length(linesWithoutNothing.x))
+    #println(linesWithoutNothing.y[end-2:end])
+    
+    return linesWithoutNothing
 end
 
-app=dash()
-app.title="kata1"
-app.layout=html_div() do
+get_trace(file)
+
+function get_number()
+    file = readlines("kata1curve.csv")[2:end-1]
+    newfile = [0,0.0]
+    for line in file
+        newline = split(line,",")
+        a = parse(Int64,newline[1])
+        b = parse(Float64,newline[2])
+        newfile = cat(newfile,[a,b],dims=2)
+    end
+    println(newfile)
+    return newfile
+end
+
+function trace()
+    v = get_number()
+    scatter(
+        x = v[1,:],
+        y = v[2,:],
+        mode = "markers",
+        name = "kata1 data-elo curve"
+    )
+end
+
+function layout(style)
+    Layout(xaxis_type=style)
+end
+function plot(style)
+    Plot(
+        trace(),
+        layout(style)
+    )
+end
+
+#= example code
+function plot(style)
+    x = range(1, stop=200, length=30)
+    Plot(
+        scatter(x=x, y=x.^3, range_x=[0.8, 250], mode="markers"),
+        Layout(
+            yaxis_type="linear",
+            xaxis_type=style,
+            xaxis_range=[log10(0.8), log10(250)]
+        )
+    )
+end=#
+
+app = dash()
+app.title = "KataGo models' plot"
+app.layout = html_div() do
     dcc_dropdown(id="style",
         options=[
-            (label="log",value="log"),
-            (label="linear",value="linear")
+            (label="log", value="log"),
+            (label="linear", value="linear")
         ],
         multi=false,
-        value="linear"
+        value="linear"  # default(init) option
     ),
     dcc_graph(id="curve") 
 end
 
 callback!(app,
     Output("curve","figure"),
-    Input("style","value"),) do val
-    p(val)
+    Input("style","value"),
+    ) do val
+    plot(val)
 end
     
 run_server(app,"0.0.0.0",8050,debug=true)
--- models.jl
+++ models.jl
@@ -9,15 +9,15 @@ function get_trace(file::String)
             continue
         else
             lineSplit = split(line.match)
-            name = match(r"b.{3,5}\d",lineSplit[1]).match
+            nnStructure = match(r"b.{3,5}\d",lineSplit[1]).match
             dataRows = match(r"d.{2,}",lineSplit[1]).match[2:end]
-            #line = (lineSplit[1],lineSplit[5])
-            line = (name,dataRows,lineSplit[end])
+            line = (name=nnStructure,x=dataRows,y=lineSplit[end])
             linesWithoutNothing = cat(linesWithoutNothing,line,dims=1)
         end
     end
-    println(length(linesWithoutNothing))
-    println(linesWithoutNothing[end-2:end])
+    #println(length(linesWithoutNothing))
+    #println(linesWithoutNothing[end-2:end])
+    return linesWithoutNothing
 end
 
 get_trace(file)
