9f3d8f3230d15fdc482e5d7d1f24918605218e32
Ya Tienna <57677458+HackYardo@users.noreply.github.com>
2022-08-27 08:24:19 +0800

    add bot_ready(); add ::Type

--- src/terminal.jl
+++ src/terminal.jl
@@ -3,10 +3,25 @@ function bot_get()
     LEELAZ = (dir="../lzweights/", cmd="leelaz --cpu-only -g -v 8 -w w6.gz")
     KATAGO = (dir="../katago1.11avx2/", cmd="./katago gtp -model \
         models/m6.txt.gz")
-    botDict = Dict("gnugo"=>GNUGO, "leelazero"=>LEELAZ, "katago"=>KATAGO)
+    botDict = Dict("g"=>GNUGO, "l"=>LEELAZ, "k"=>KATAGO)
     
     botDict[ARGS[1]]
 end 
+
+function bot_ready(proc::Base.Process)
+    query(proc, "!")
+    outInfo = reply(proc)
+    
+    if outInfo[1] != '?'
+        errInfo = reply(proc.err)
+        @info "stdout:\n$outInfo"
+        @info "stderr:\n$errInfo"
+        @error "Please look at the above ↑↑↑"
+        exit()
+    end
+
+    println("$proc")
+end
 
 #=
 Why Base.PipeEndpoint() && run() or why not open()?
@@ -14,29 +29,21 @@ Because stderr is hard to talk with, source:
 https://discourse.julialang.org/t/avoiding-readavailable-when-communicating-
 with-long-lived-external-program/61611/25
 =#
-function bot_run(; dir="", cmd="")
+function bot_run(; dir="", cmd="")::Base.Process
     inp = Base.PipeEndpoint()
     out = Base.PipeEndpoint()
     err = Base.PipeEndpoint()
     
     cmdVector = split(cmd) # otherwise there will be ' in command
     command = Cmd(`$cmdVector`, dir=dir)
-    cmdString = "$command"
-    println("Julia will run the command:\n$cmdString")
-    
+    println("VastGo will run the command \n\t$cmd\nin the direetory\n\t$dir")
+    #println(command)
+    println("Please waiting...")
+
     process = run(command,inp,out,err;wait=false)
     
-    query(process, "name")
-    name = reply(process)
-    if name[1] != '='
-        errInfo = readchomp(process.err)
-        @info "stdout:\n$name"
-        @info "stderr:\n$errInfo"
-        @error "Please look at the above ↑↑↑"
-        exit()
-    end
+    bot_ready(process)
     
-    #println("$process")
     return process
 end
 
@@ -45,7 +52,7 @@ function bot_end(proc::Base.Process)
     close(proc)
 end
 
-function isvalid(sentence::String)
+function isvalid(sentence::String)::Bool
     if "" in split(sentence, keepempty=true)
         return false
     else 
@@ -53,26 +60,26 @@ function isvalid(sentence::String)
     end 
 end 
 
-function query(proc, sentence::String)
+function query(proc::Base.Process, sentence::String)
     println(proc, sentence)
 end
 
-function reply(proc)
+function reply(proc::Union{Base.Process, Base.PipeEndpoint})::String
     paragraph = readuntil(proc, "\n\n")
     return "$paragraph\n"
 end
 
-function name_get(proc)
+function name_get(proc::Base.Process)::SubString
     query(proc, "name")
     reply(proc)[3:end-1]
 end
 
-function version_get(proc)
+function version_get(proc::Base.Process)::SubString
     query(proc, "version")
     reply(proc)[3:end-1]
 end 
 
-function gtp_startup_info(proc)
+function gtp_startup_info(proc::Base.Process)
     name = name_get(proc)
     if name == "Leela Zero"
         info = readuntil(proc.err, "MiB.", keep=true)
@@ -84,12 +91,12 @@ function gtp_startup_info(proc)
     println(info)
 end 
 
-function gtp_ready(proc)
+function gtp_ready(proc::Base.Process)
     gtp_startup_info(proc)
     @info "GTP ready"
 end 
 
-function leelaz_showboard(proc)
+function leelaz_showboard(proc::Base.Process)::String
     readuntil(proc.err, "Passes:")
     paragraphErr = "Passes:" * readuntil(proc.err, "\n") * "\n"
     while true
@@ -105,7 +112,7 @@ function leelaz_showboard(proc)
     paragraphErr
 end
 
-function showboard_get(proc::Base.Process)
+function showboard_get(proc::Base.Process)::Tuple
     paragraph = reply(proc)
     name = name_get(proc)
     if name == "Leela Zero"
@@ -115,8 +122,12 @@ function showboard_get(proc::Base.Process)
     paragraph, name
 end 
 
-function showboard_format(proc)
+function showboard_format(proc::Base.Process)
     paragraph, name = showboard_get(proc)
+end
+
+function gaming()
+    
 end
 
 function terminal()
